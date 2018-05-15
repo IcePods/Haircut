bec74f7cfca14b74c0fd7db747311da8e9238a1a
liliyaxin <liyaxin2015com@163.com>
2018-05-15 15:04:00 +0800

    Revert "PersonFragment"
    This reverts commit be1c6b02cc2ca48317f3862c20ad0b58c2a659c1.

--- app/src/main/java/com/example/lu/thebarbershop/Fragment/PersonFragment.java
+++ app/src/main/java/com/example/lu/thebarbershop/Fragment/PersonFragment.java
@@ -1,7 +1,6 @@
 package com.example.lu.thebarbershop.Fragment;
 
 import android.app.Fragment;
-import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Paint;
@@ -15,13 +14,8 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 
-import com.example.lu.thebarbershop.Activity.UserPersonCollectionActivity;
-import com.example.lu.thebarbershop.Activity.UserPersonInformationActivity;
-import com.example.lu.thebarbershop.MyTools.GetRoundedCornerBitmap;
 import com.example.lu.thebarbershop.R;
 
 /**
@@ -30,61 +24,42 @@ import com.example.lu.thebarbershop.R;
  */
 
 public class PersonFragment extends Fragment {
-    private ImageView imageView;//头像
-    private Button infobutton;//个人信息按钮 id=user_person_information_btn
-    private Button collectionbutton;//我的收藏按钮 id=user_person_collection_btn
-    private Mylistener mylistener;//监听器
+    private ImageView imageView;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         //加载选项卡对应的选项页面
         View view = inflater.inflate(R.layout.fragment_person,container,false);
 
-        imageView = view.findViewById(R.id.user_person_head_portrait_img);//头像
-        infobutton = view.findViewById(R.id.user_person_information_btn);//个人信息按钮
-        collectionbutton = view.findViewById(R.id.user_person_collection_btn);//我的收藏按钮
-
-        mylistener = new Mylistener();
-
-        imageView.setImageBitmap(GetRoundedCornerBitmap.getRoundedCornerBitmap(((BitmapDrawable)imageView.getDrawable()).getBitmap(),2));
-        infobutton.setOnClickListener(mylistener);
-        collectionbutton.setOnClickListener(mylistener);
-
+        imageView = view.findViewById(R.id.user_person_head_portrait_img);
+        imageView.setImageBitmap(getRoundedCornerBitmap(((BitmapDrawable)imageView.getDrawable()).getBitmap(),2));
         return view;
     }
 
-    //监听器
-    private class Mylistener implements View.OnClickListener{
+    /**
+     * 设置圆角图片 当ratio为2时，为圆形图片
+     * @param bitmap 原bitmap图片
+     * @param ratio
+     * @return
+     */
+    public static Bitmap getRoundedCornerBitmap(Bitmap bitmap, float ratio) {
 
-        @Override
-        public void onClick(View v) {
-            //1. 创建Intent对象
-            Intent intent = new Intent();
-            switch (v.getId()){
-                //个人信息按钮
-                case R.id.user_person_information_btn:
-                    //只实现跳转，跳转到个人信息详情页面UserPersonInformationActivity
-                    //2. 指定跳转路线
-                    intent.setClass(getActivity().getApplicationContext(),UserPersonInformationActivity.class);
-                    //3. 进行跳转
-                    startActivity(intent);
-                    break;
-                //我的预约按钮
-                case R.id.user_person_appointment_btn:
-                    break;
-                //我的收藏按钮
-                case R.id.user_person_collection_btn:
-                    //只实现跳转，跳转到我的收藏页面UserPersonCollectionActivity
-                    //2. 指定跳转路线
-                    intent.setClass(getActivity().getApplicationContext(),UserPersonCollectionActivity.class);
-                    //3. 进行跳转
-                    startActivity(intent);
-                    break;
-                //预约人信息按钮
-                case R.id.user_person_appoint_information_btn:
-                    break;
-            }
-        }
+        Bitmap output = Bitmap.createBitmap(bitmap.getWidth(),
+                bitmap.getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(output);
+
+        final Paint paint = new Paint();
+        final Rect rect = new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight());
+        final RectF rectF = new RectF(rect);
+
+        paint.setAntiAlias(true);
+        canvas.drawARGB(0, 0, 0, 0);
+        canvas.drawRoundRect(rectF, bitmap.getWidth() / ratio,
+                bitmap.getHeight() / ratio, paint);
+
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        canvas.drawBitmap(bitmap, rect, rect, paint);
+        return output;
     }
 }
 
