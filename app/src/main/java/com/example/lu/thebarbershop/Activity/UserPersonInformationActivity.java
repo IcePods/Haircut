1021cd663498e1e1ebba8d40c25421193889fd8b
1036862114 <1036862114@qq.com>
2018-05-22 15:38:32 +0800

    增加个人头像裁剪和从相册中选择功能
    增加个人头像裁剪和从相册中选择功能(仍然不能保存为默认头像)

--- app/src/main/java/com/example/lu/thebarbershop/Activity/UserPersonInformationActivity.java
+++ app/src/main/java/com/example/lu/thebarbershop/Activity/UserPersonInformationActivity.java
@@ -4,6 +4,9 @@ import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -14,10 +17,12 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.PopupWindow;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
-import com.example.lu.thebarbershop.MyTools.CenterSquareScaleBitmap;
 import com.example.lu.thebarbershop.MyTools.GetRoundedCornerBitmap;
 import com.example.lu.thebarbershop.R;
+
+import java.io.File;
 
 public class UserPersonInformationActivity extends AppCompatActivity {
     private ImageView imageView;//头像
@@ -29,8 +34,17 @@ public class UserPersonInformationActivity extends AppCompatActivity {
     private Button exittologin;//退出登录
 
     private PopupWindow popupWindow;
-    private final int CAMERA_REQUEST = 8888;
-
+    //------------------------------------------------
+    /* 头像文件 */
+    private static final String IMAGE_FILE_NAME = "temp_head_image.jpg";
+    /* 请求识别码 */
+    private static final int CODE_GALLERY_REQUEST = 0xa0;
+    private static final int CODE_CAMERA_REQUEST = 0xa1;
+    private static final int CODE_RESULT_REQUEST = 0xa2;
+    // 裁剪后图片的宽(X)和高(Y),480 X 480的正方形。
+    private static int output_X = 480;
+    private static int output_Y = 480;
+    //-------------------------------------------------------
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -114,7 +128,7 @@ public class UserPersonInformationActivity extends AppCompatActivity {
         Button btn_choose = v.findViewById((R.id.chooseFrom));
         Button btn_cancel = v.findViewById(R.id.cancel);
         btn_take.setOnClickListener(new OnClickListener());
-
+        btn_choose.setOnClickListener(new OnClickListener());
         btn_cancel.setOnClickListener(new OnClickListener() );
 
         popupWindow=new PopupWindow(this);
@@ -150,8 +164,11 @@ public class UserPersonInformationActivity extends AppCompatActivity {
         public void onClick(View view) {
             switch (view.getId()){
                 case R.id.takePicture:
-                    Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-                    startActivityForResult(cameraIntent, CAMERA_REQUEST);
+                    choseHeadImageFromCameraCapture();
+                    popupWindow.dismiss();
+                    break;
+                case R.id.chooseFrom:
+                    choseHeadImageFromGallery();
                     popupWindow.dismiss();
                     break;
                 case R.id.cancel:
@@ -162,13 +179,110 @@ public class UserPersonInformationActivity extends AppCompatActivity {
             }
         }
     }
-    //返回拍的照片并设置给imageview
+    // 从本地相册选取图片作为头像
+    private void choseHeadImageFromGallery() {
+        Intent intentFromGallery = new Intent();
+        // 设置文件类型
+        intentFromGallery.setType("image/*");
+        intentFromGallery.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intentFromGallery, CODE_GALLERY_REQUEST);
+    }
 
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == CAMERA_REQUEST && resultCode == RESULT_OK) {
-            Bitmap photo = (Bitmap) data.getExtras().get("data");
-            Bitmap result = CenterSquareScaleBitmap.centerSquareScaleBitmap(photo,80);
-            imageView.setImageBitmap(GetRoundedCornerBitmap.getRoundedCornerBitmap(result,2));
+    // 启动手机相机拍摄照片作为头像
+    private void choseHeadImageFromCameraCapture() {
+        Intent intentFromCapture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        // 判断存储卡是否可用，存储照片文件
+        if (hasSdcard()) {
+            intentFromCapture.putExtra(MediaStore.EXTRA_OUTPUT, Uri
+                    .fromFile(new File(Environment
+                            .getExternalStorageDirectory(), IMAGE_FILE_NAME)));
+        }
+
+        startActivityForResult(intentFromCapture, CODE_CAMERA_REQUEST);
+    }
+    /**
+     * 裁剪原始的图片
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,
+                                    Intent intent) {
+
+        // 用户没有进行有效的设置操作，返回
+        if (resultCode == RESULT_CANCELED) {
+            Toast.makeText(getApplication(), "取消", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        switch (requestCode) {
+            case CODE_GALLERY_REQUEST:
+                cropRawPhoto(intent.getData());
+                break;
+
+            case CODE_CAMERA_REQUEST:
+                if (hasSdcard()) {
+                    File tempFile = new File(
+                            Environment.getExternalStorageDirectory(),
+                            IMAGE_FILE_NAME);
+                    cropRawPhoto(Uri.fromFile(tempFile));
+                } else {
+                    Toast.makeText(getApplication(), "没有SDCard!", Toast.LENGTH_LONG)
+                            .show();
+                }
+
+                break;
+
+            case CODE_RESULT_REQUEST:
+                if (intent != null) {
+                    setImageToHeadView(intent);
+                }
+
+                break;
+        }
+
+        super.onActivityResult(requestCode, resultCode, intent);
+    }
+    public void cropRawPhoto(Uri uri) {
+
+        Intent intent = new Intent("com.android.camera.action.CROP");
+        intent.setDataAndType(uri, "image/*");
+
+        // 设置裁剪
+        intent.putExtra("crop", "true");
+
+        // aspectX , aspectY :宽高的比例
+        intent.putExtra("aspectX", 1);
+        intent.putExtra("aspectY", 1);
+
+        // outputX , outputY : 裁剪图片宽高
+        intent.putExtra("outputX", output_X);
+        intent.putExtra("outputY", output_Y);
+        intent.putExtra("return-data", true);
+
+        startActivityForResult(intent, CODE_RESULT_REQUEST);
+    }
+
+    /**
+     * 提取保存裁剪之后的图片数据，并设置头像部分的View
+     */
+    private void setImageToHeadView(Intent intent) {
+        Bundle extras = intent.getExtras();
+        if (extras != null) {
+            Bitmap photo = extras.getParcelable("data");
+            imageView.setImageBitmap(GetRoundedCornerBitmap.getRoundedCornerBitmap(photo,2));
+        }
+    }
+
+    /**
+     * 检查设备是否存在SDCard的工具方法
+     */
+    public static boolean hasSdcard() {
+        String state = Environment.getExternalStorageState();
+        if (state.equals(Environment.MEDIA_MOUNTED)) {
+            // 有存储的SDCard
+            return true;
+        } else {
+            return false;
         }
     }
 }
